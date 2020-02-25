4bfee5df8697545d6d1dddd1d27e9a1164eaac82
Ian <50344364+ihugya1@users.noreply.github.com>
2020-02-25 15:49:30 -0700

    Began table view

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -8,14 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource {
     var gameValue = 0;
     var playerValue = 0;
     var winCount=0;
+    private let dwarves = ["Ian", "Tyler"]
+    let simpleTableIdentifier = "test"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //MARK:-
+    //Market: Table View Data Source Methods
 
     @IBOutlet weak var rockButton: UIButton!
     @IBOutlet weak var paperButton: UIButton!
@@ -23,8 +27,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var theLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+   
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        if(cell==nil){
+            cell=UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: simpleTableIdentifier)
+        }
+        cell?.textLabel?.text=dwarves[indexPath.row]
+        return cell!
+    }
+    
+    @IBAction func displayActionSheet(_ sender: Any) {
+        
+        
+    }
     @IBAction func rockAction(_ sender: Any) {
         randomGame();
         playerValue=1;
@@ -37,7 +57,20 @@ class ViewController: UIViewController {
               theLabel.text="Loss"
             winCount=0;
              scoreLabel.text=String(winCount);
+            // 1
+            let optionMenu = UIAlertController(title: nil, message: "You lost", preferredStyle: .actionSheet)
+            
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            // 4
+            
+            optionMenu.addAction(cancelAction)
+            
+            // 5
+            self.present(optionMenu, animated: true, completion: nil)
         }
+     
     }
     
     @IBAction func paperAction(_ sender: Any) {
@@ -52,7 +85,19 @@ class ViewController: UIViewController {
         else{
             theLabel.text="Loss"
             winCount=0;
-             scoreLabel.text=String(winCount);
+            scoreLabel.text=String(winCount);
+            // 1
+            let optionMenu = UIAlertController(title: nil, message: "You lost", preferredStyle: .actionSheet)
+            
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            // 4
+           
+            optionMenu.addAction(cancelAction)
+            
+            // 5
+            self.present(optionMenu, animated: true, completion: nil)
         }
         
     }
@@ -68,6 +113,18 @@ class ViewController: UIViewController {
               theLabel.text="Loss"
             winCount=0;
              scoreLabel.text=String(winCount);
+            // 1
+            let optionMenu = UIAlertController(title: nil, message: "You lost", preferredStyle: .actionSheet)
+            
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            // 4
+           
+            optionMenu.addAction(cancelAction)
+            
+            // 5
+            self.present(optionMenu, animated: true, completion: nil)
         }    }
     
     func randomGame(){
